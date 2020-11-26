3573de60952b4343537ab4b0d31c7a86f49b9d46
joel <joel.toula@gmail.com>
2020-11-26 12:39:12 -0300

    test

--- Tobi/Serv/todo.bash
+++ Tobi/Serv/todo.bash
@@ -5,3 +5,8 @@ FLUSH PRIVILEGES;
 pip3 install PyQt5
 sudo apt-get install python3-pip python3-dev  
 sudo apt-get install git
+git config --global user.name "joel"
+git config --global user.email "joel.toula@gmail.com"
+git remote add origin SSH git@github.com:Mav-Joel/projet.git
+git branch -M main
+git push -u origin main --verbose
